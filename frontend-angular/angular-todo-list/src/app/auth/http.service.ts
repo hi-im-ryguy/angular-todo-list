b8cd664a304ff1feac823a66db0036e94e4c20fd
Ryan L <luong_psn@yahoo.com>
2021-11-09 00:18:04 -0800

    Basic Http Service INIT

---
+++ frontend-angular/angular-todo-list/src/app/auth/http.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientJsonpModule, HttpErrorResponse, HttpParams, HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  private REST_API_SERVER_URL = "https://39fjkh53ck.execute-api.us-west-1.amazonaws.com/default/to-do-list-rest-api";
+
+  constructor(private httpClient: HttpClient) { }
+
+  public sendGetRequest() {
+    return this.httpClient.get(this.REST_API_SERVER_URL);
+  }
+}
