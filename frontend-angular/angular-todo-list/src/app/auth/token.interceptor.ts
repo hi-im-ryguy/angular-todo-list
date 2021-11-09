4a52ae082562c36a550c64a3a96cf8d36b4264e5
Ryan L <luong_psn@yahoo.com>
2021-11-09 00:17:32 -0800

    Token Interceptor INIT

---
+++ frontend-angular/angular-todo-list/src/app/auth/token.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+// import { AuthService } from './auth/auth.service';
+import { Observable } from 'rxjs';
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor() {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    
+    request = request.clone({
+      setHeaders: {
+        Authorization: `Bearer OTdOeet56BacQONhn2mDW5rILR2Ty5An1GSPg8h3`
+      }
+    });
+    return next.handle(request);
+  }
+}
